e611927a1a2c4c32c5ae3a7a51eefc3043e2a3e7
AlandSleman <alandsazkm@gmail.com>
2023-10-01 22:50:28 +0300

    add icons

--- website/src/app/(home)/page.tsx
+++ website/src/app/(home)/page.tsx
@@ -1,8 +1,14 @@
-import Link from "next/link"
-import { GithubIcon } from "lucide-react"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import AnsibleIcon from "@/components/icons/Ansible"
+import DockerIcon from "@/components/icons/Docker"
+import GoIcon from "@/components/icons/Go"
+import GrafanaIcon from "@/components/icons/Grafana"
+import NextIcon from "@/components/icons/Next"
+import NginxIcon from "@/components/icons/Nginx"
+import PostgresIcon from "@/components/icons/Postgres"
+import PrismaIcon from "@/components/icons/Prisma"
+import PrometheusIcon from "@/components/icons/Prometheus"
+import ReactQueryIcon from "@/components/icons/ReactQuery"
+import RedisIcon from "@/components/icons/Redis"
 
 export default function Page() {
   return (
@@ -20,33 +26,42 @@ export default function Page() {
           <img className="max-w-[900px]" src="/dashboard.png" />
         </div>
 
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between items-center w-full">
           <div className="flex flex-col">
             <h1 className="text-2xl font-extrabold leading-tight tracking-tighter md:text-3xl">
               Powered by a Powerful Tech Stack
             </h1>
           </div>
-          <div className="flex">
-            <GithubIcon />
-            <GithubIcon />
-            <GithubIcon />
-            <GithubIcon />
-            <GithubIcon />
+          <div className="flex flex-col items-center gap-4">
+          <div className="flex items-center gap-4">
+            <NextIcon className="w-20 h-20" />
+            <ReactQueryIcon className="w-20 h-20" />
+            <GoIcon className="w-24 h-24" />
+            <PrismaIcon className="w-32 h-32" />
+            <PostgresIcon className="w-20 h-20" />
+            <RedisIcon className="w-20 h-20" />
+          </div>
+          <div className="flex items-center gap-4">
+            <GrafanaIcon className="w-20 h-20" />
+            <PrometheusIcon className="w-20 h-20" />
+            <NginxIcon className="w-20 h-20" />
+            <DockerIcon className="w-20 h-20" />
+            <AnsibleIcon className="w-20 h-20" />
+          </div>
           </div>
         </div>
 
         <div className="flex justify-between w-full">
           <div className="flex flex-col">
             <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-Admin Dashboard
+              Admin Dashboard
             </h1>
             <p className="text-lg text-muted-foreground">
-Admin Dashboard with Useful Metrics and Statistics
+              Admin Dashboard with Useful Metrics and Statistics
             </p>
           </div>
           <img className="max-w-[900px]" src="/dashboard.png" />
         </div>
-
       </div>
     </section>
   )
---
+++ website/src/components/icons/Docker.tsx
@@ -0,0 +1,23 @@
+
+import * as React from "react"
+import { SVGProps } from "react"
+const DockerIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-label="Docker"
+    viewBox="0 0 512 512"
+    {...props}
+  >
+    <rect width={512} height={512} fill="#fff" rx="15%" />
+    <path
+      stroke="#066da5"
+      strokeWidth={38}
+      d="M296 226h42m-92 0h42m-91 0h42m-91 0h41m-91 0h42m8-46h41m8 0h42m7 0h42m-42-46h42"
+    />
+    <path
+      fill="#066da5"
+      d="M472 228s-18-17-55-11c-4-29-35-46-35-46s-29 35-8 74c-6 3-16 7-31 7H68c-5 19-5 145 133 145 99 0 173-46 208-130 52 4 63-39 63-39"
+    />
+  </svg>
+)
+export default DockerIcon
